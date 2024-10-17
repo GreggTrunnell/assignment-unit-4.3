a2ec91e0b78a44bbb4d37f2201cae9462cf3ca4a
Gregg Trunnell <gggtrunnell@gmail.com>
2024-10-16 22:36:48 -0500

    first save

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,11 +2,27 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket=[];
 
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+addItem('butter');
+addItem('milk');
+addItem('apples');
+console.log('The basket has ' + basket + ' in it');
+console.log(basket);
 
+function listItems(){
+    for (let i = 0; i < basket.length; i++)
+        console.log(basket[i]);
+    }
 
-
-
+function empty(basket){
+    for (let i = 0; i < basket.length; i++)
+        basket.shift[i];
+}
 
 
 
