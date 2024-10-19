041758b0f2cc3c3ad8dd85430b9569bb8c91d0c3
Gregg Trunnell <gggtrunnell@gmail.com>
2024-10-19 13:49:25 -0500

    submit

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -8,25 +8,34 @@ function addItem(item){
     basket.push(item);
     return true;
 }
-addItem('butter');
-addItem('milk');
-addItem('apples');
-console.log('The basket has ' + basket + ' in it');
-console.log(basket);
-
-function listItems(){
-    for (let i = 0; i < basket.length; i++)
-        console.log(basket[i]);
-    }
-
-function empty(basket){
-    for (let i = 0; i < basket.length; i++)
-        basket.shift[i];
+console.log(`Basket is ${basket}`);
+console.log('Added apples ', addItem('apples'));
+console.log(`Basket now has ${basket.join(' and ')} in it.`);
+console.log('Added milk',addItem('milk'))
+console.log(`I added another item so now I should have ${basket.join(' and ')} in it.`)
+function currentBasket(){
+    console.log(`My basket has ${basket.join(' and ')} in it.`);
 }
 
 
+addItem('fruit');
+currentBasket();
 
+function listItems(){
+    for (let items = 0; items < basket.length; items++)
+        console.log(basket[items]);
+    }
+console.log(listItems());
 
+function empty(){
+    basket.splice(0,basket.length);
+}
+console.log('Is this basket empty?', empty());
+
+addItem('bread')
+addItem('soda');
+console.log(basket);
+currentBasket();
 
 // DO NOT MODIFY
 // Used for automated testing
